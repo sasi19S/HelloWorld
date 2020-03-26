b34ce42aa163c93ca59b00b7549a324a55c1a40f
sasi19S <55355792+sasi19S@users.noreply.github.com>
2020-03-26 15:22:30 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 1920!" );
+        System.out.println( "Hello World 1SS!" );
     }
 }
